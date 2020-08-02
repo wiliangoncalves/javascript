bc21b11d5a929c8c22fdf203599ccecdb9cc290d
wiliangoncalves <63382506+wiliangoncalves@users.noreply.github.com>
2020-08-01 23:53:56 -0300

    ifElseif.js

---
+++ controle/ifElseif.js
@@ -0,0 +1,33 @@
+Number.prototype.entre = function (inicio, fim){
+    return this >= inicio && this <= fim
+}
+
+const resultado = function(nota){
+    if(nota.entre(9, 10)) 
+    {
+        console.log('Quadro de Honra')
+    }
+    else if(nota.entre(7, 8.99)) 
+    {
+        console.log('Aprovado!')
+    }
+    else if(nota.entre(4, 6.99)) 
+    {
+        console.log('Recuperação!')
+    }
+    else if(nota.entre(0, 3.99))
+    {
+        console.log('Reprovado!')
+    }
+    else {
+        console.log('Nota inválida!')
+    }
+
+    console.log('Fim!')
+}
+resultado(10)
+resultado(8.9)
+resultado(6.55)
+resultado(2.3)
+resultado(-1)
+resultado(11)
