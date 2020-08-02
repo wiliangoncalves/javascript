b1ff3ae127e6316d2a11f44563269ed48bc55110
wiliangoncalves <63382506+wiliangoncalves@users.noreply.github.com>
2020-08-01 23:53:28 -0300

    ifElse.js

---
+++ controle/ifElse.js
@@ -0,0 +1,11 @@
+const resultado = function(nota){
+    if(nota >= 7){
+        console.log('Aprovado')
+    } 
+    else {
+        console.log('Reprovado')
+    }
+}
+resultado(10)
+resultado(4)
+resultado('Epa!') // CUIDADO.
