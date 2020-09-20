05738a3bd5c6e4488dc0396b62bdf33ec56844f6
wilian goncalves <sensui289@gmail.com>
2020-09-20 01:35:11 -0300

    execicio3.js

---
+++ funcao/exercicios/exercicio3.js
@@ -0,0 +1,5 @@
+/* Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente */
+function baseleavo(base, expoente){
+    return console.log(`${base} elevado a ${expoente} = ${base * expoente}`)
+}
+baseleavo(4,2)
