327997ef9814a3d0a5ebcd753639648313d90f8c
wilian goncalves <sensui289@gmail.com>
2020-12-08 02:17:07 -0300

    git commit -m"deletado"

--- logicaJS/aula01/script.js
+++
@@ -1,7 +0,0 @@
-let nome = prompt('Qual seu nome?');
-alert('Bem vindo, ' + `${nome}`);
-
-let endereco = 'string'
-let numerointeiro = 1000
-let numeroreal = 2.5
-let boleano = 'Verdadeiro, true ou falso, false'
