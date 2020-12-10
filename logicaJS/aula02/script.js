ee3e0395f2eb6420984590ba46c54596de04539e
wilian goncalves <sensui289@gmail.com>
2020-12-10 14:25:14 -0300

    logicajs

---
+++ logicaJS/aula02/script.js
@@ -0,0 +1,4 @@
+//Pedir para o usuário digitar um número e mais um número e então somar
+let numero1 = Number(prompt('Digite um número'));
+let numero2 = Number(prompt('Digite mais um número'));
+alert('A soma entre ' + numero1 + '+ ' + numero2 + '= ' + (numero1 + numero2));
