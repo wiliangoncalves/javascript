6491c81bf33353d02cad54290180d362147ff517
wilian gonçalves <sensui289@gmail.com>
2021-01-10 00:46:05 -0300

    ecmaScript2009

---
+++ ecmaScript2009/sistema_lexico1/script.js
@@ -0,0 +1,21 @@
+let nome = 'wilian';
+let n = 10;
+let n2 = 1.5;
+let ehMaiorDeIdade = true;
+let obj = new Object();
+let frutas = ['maçã', 'pera', 'uva']; // Literal de um array.
+let frase = 'O "HTML" é uma linguagem de marcação';
+let mensagem = "Bem vindo ao mundo do\" HTML\" divirta-se";
+
+obj.nome = 'joão';
+obj = {
+    nome: 'wilian',
+    idade: 21
+};
+
+alert(nome);
+
+//alert('olá, mundo!')
+/*
+Comentário de múltiplas linhas.
+*/
---
+++ ecmaScript2009/sistema_lexico2/script.js
@@ -0,0 +1,12 @@
+var primeiroTipo = 'Variável com var';
+let segundoTipo  = 'Variável com let';
+const constante = 'Criar uma constante com const';
+let teste;
+let teste2 = null;
+let n = 10;
+let str = ' olá ';
+
+alert(n * str);
+alert(n + str);
+//alert(teste);
+//alert(teste2);
