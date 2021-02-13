596e5746ebef22c6963a4aa8fa39f205f57b670f
wilian gonçalves <sensui289@gmail.com>
2021-02-13 17:54:00 -0300

    heranca02

---
+++ ecmaScript2015/objeto/heranca02/heranca02.js
@@ -0,0 +1,45 @@
+// Cadeia de Protótipos (Prototype chain)
+Object.prototype.attr0 = 'Z'; // Não faça isso!
+const avo = {attr1: 'A'};
+const pai = {__proto__: avo, atrr2: 'B'};
+const filho = {__proto__: pai, attr3: 'C', attr3: 'C'};
+console.log(filho.attr0, filho.attr1, filho.atrr2, filho.attr3);
+
+const carro = {
+    velAtual: 0,
+    velMax: 200,
+    aceleraMais(delta){
+        if(this.velAtual + delta <= this.velMax){
+            this.velAtual += delta;
+        } else{
+            this.velAtual = this.velMax;
+        }
+    },
+    status(){
+        return `${this.velAtual}Km/h de ${this.velMax}Km/h`;
+    }
+}
+
+const ferrari = {
+    modelo: 'F40',
+    velMax: 324 //Shadowing.
+}
+
+const volvo = {
+    modelo: 'V40',
+    status(){
+        return `${this.modelo}: ${super.status()}`;
+    }
+}
+
+Object.setPrototypeOf(ferrari, carro);
+Object.setPrototypeOf(volvo, carro);
+
+console.log(ferrari);
+console.log(volvo);
+
+volvo.aceleraMais(100);
+console.log(volvo.status());
+
+ferrari.aceleraMais(300);
+console.log(ferrari.status());
