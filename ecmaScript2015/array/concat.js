cd6da041317dc0f3d46ee5d2931370fac640c26a
wilian gonçalves <sensui289@gmail.com>
2021-03-12 12:37:36 -0300

    concat.js

---
+++ ecmaScript2015/array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Karen', 'Triggered'];
+const filhos = ['Jhon', 'Dutch'];
+const todos = filhos.concat(filhos, 'Adicionado');
+
+console.log(todos);
+
+console.log([].concat([1, 2], [3, 4], 5, [[6,7]]));
