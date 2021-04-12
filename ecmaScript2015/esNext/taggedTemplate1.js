ecad2ec13885dabe9b3a08c006f97426a7e5d2f8
wilian gonçalves <sensui289@gmail.com>
2021-04-11 22:51:16 -0300

    taggedTemplate1.js

---
+++ ecmaScript2015/esNext/taggedTemplate1.js
@@ -0,0 +1,10 @@
+// tagged template - processar uma template dentro de uma função.
+function tag(partes, ...valores){
+    console.log(partes);
+    console.log(valores);
+    return 'Outra string';
+}
+
+const aluno = 'Gui';
+const situacao = 'Aprovado';
+console.log(tag `${aluno} está ${situacao}.`);
